77c92a938069b9d4ace259bd424b1371cd3d0bb9
Hector Eddin Acosta Najera <hector889ju@gmail.com>
2022-03-28 19:14:45 -0600

    Removed React Logo from Title

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import './styles.css';
+import { AiFillTwitterCircle,AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 function Header() {
     return (
         <header>
             <h1>TodoApp</h1>
+
+            <div className="contact-info">
+                <a href="https://twitter.com/hector889ju">
+                    <AiFillTwitterCircle className="icon"/>
+                </a>
+                <a href="https://github.com/ThunderProCode">
+                    <AiFillGithub className="icon"/>
+                </a>
+                <a href="https://www.linkedin.com/in/hector-acosta-najera-6a3643144/">
+                    <AiFillLinkedin className="icon"/>
+                </a>
+            </div>
+
         </header>
     );
 }
